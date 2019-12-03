f967e1a2be7e8257a27d83abc3a65774f6e7a22f
Wooyon <48887513+Wooyon@users.noreply.github.com>
2019-12-04 00:30:32 +0900

    #2.1 custom icons for the tab navigation

--- constants/Colors.js
+++ constants/Colors.js
@@ -2,4 +2,4 @@ export const BG_COLOR = "black";
 export const WHIT = "white";
 export const GREY_COLOR = "#bdc3c7";
 export const INACTIVE_COLOR = "#7f8c8d";
-export const ACTIVE_COLOR = "whit";
+export const ACTIVE_COLOR = "white";
